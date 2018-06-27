7c57d9eca634c078ed77a8922b5dafc38f36b4a0
spiros2015 <spiros2015@163.com>
2018-06-27 11:26:56 +0800

    Update letterCombinations.js

--- letterCombinations.js
+++ letterCombinations.js
@@ -22,3 +22,42 @@ var letterCombinations = function(digits) {
     }
     return res;
 }
+
+//递归
+// var letterCombinations = function(digits) {
+//     let letters={
+//         '2':'abc',
+//         '3':'def',
+//         '4':'ghi',
+//         '5':'jkl',
+//         '6':'mno',
+//         '7':'pqrs',
+//         '8':'tuv',
+//         '9':'wxyz'
+//     };
+//     let ret = [];
+//     for(let letter of digits){
+//         ret.push(letters[letter])
+//     }
+//     return binaryCombination(ret,0,digits.length);
+// };
+
+// function binaryCombination(ret,low,high){
+//     let len = high-low;
+//     let res=[];
+//     if(len===1){
+//         return [...ret[low]];
+//     }
+//     if(len===0)
+//         return [];
+//     len = Math.floor(len/2);
+//     let ra =binaryCombination(ret,low,len+low);
+//     let rb =binaryCombination(ret,len+low,high);
+//     for(let a of ra){
+//         for(let b of rb){
+//             res.push(a+b);
+//         }
+//     }
+//     return res;
+
+// }
