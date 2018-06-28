0e2b8e5c6b2562625949ba9b539ab3b6d458e616
spiros2015 <spiros2015@163.com>
2018-06-28 18:01:32 +0800

    Create shellSort.js

---
+++ Datastructure/sort/shellSort.js
@@ -0,0 +1,17 @@
+let shellSort = function (nums) {
+    let len= nums.length;
+    let temp;
+    let j;
+    for (let add=Math.floor(len/2);add>0;){
+        for (let i=add;i<len;i++){
+            temp=nums[i];
+            for (j=i;j>=add&&nums[j-add]>temp;j-=add){
+                nums[j]=nums[j-add];
+            }
+            nums[j]=temp;
+        }
+        add=Math.floor(add/2)
+        console.log(nums)
+    }
+    return nums;
+};
